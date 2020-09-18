4e9472c9b9b767474ab848b93e55e80c46143cfb
Telmah254 <vader2012pl@gmail.com>
2020-09-18 23:53:21 +0200

    css

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
   const [filteredTodos, setFilteredTodos] = useState([]);
 
   useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
     filterHandler(); 
     saveLocalTodos();
   }, [todos, status]);
@@ -31,17 +35,19 @@ function App() {
   };
 
   const saveLocalTodos = () => {
-    if(localStorage.getItem('todos') === null){
-      localStorage.setItem('todos', JSON.stringify([]));
-    } else{
-      localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem('todos', JSON.stringify(todos)); 
+  };
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+    localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+  let todoLocal = JSON.parse(localStorage.getItem("todos"));
+    setTodos(todoLocal);
     }
-  }
+  };
   return (
     <div className="App">
-      <header>Todo List
-      <h1>Hello react</h1>
-      </header>
+      <h1>Todo List</h1>
       <Form 
       inputText={inputText}
       todos={todos} 
